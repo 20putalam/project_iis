048d13f5884e2f585cf6bf7e7c107a97da05a156
20putalam <marek.putala@email.cz>
2021-11-24 18:52:01 +0100

    hash

--- flasklib/__init__.py
+++ flasklib/__init__.py
@@ -4,8 +4,9 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+#app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ptokoocanzqqkx:fae86dd94df17029ffb5c3f6009c8ffedb6323fb56cc2e21e53cb6e88d29e0bf@ec2-34-241-19-183.eu-west-1.compute.amazonaws.com:5432/d655itnumnt7dq'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
