63ae1a09861e2bd694e9c223911e4692483646a8
filip <f.sapak17@gmail.com>
2021-11-27 18:15:01 +0100

    fix

--- flasklib/routes.py
+++ flasklib/routes.py
@@ -52,7 +52,7 @@ def manageusers():
                     flash("Given ID not found in database")
 
                 user = User.query.filter_by(id=form.id.data)
-                user.role = form.role
+                user.role_id = form.role
                 db.session.commit()
                 
             return render_template('manageusers.html', title='Admin Tools',users=users,form=form)
